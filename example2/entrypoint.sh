3a49ab5f85d2ce6192743e1952daba933b72f0c3
prasadtalasila <prasad.talasila@gmail.com>
2022-05-11 15:31:23 +0200

    Cleans up example2
      - Adds docker container creation steps to example2
      - Adds entry script for the container
      - Fixes the problems with cosim-model.zip file

---
+++ example2/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/cosim
+cp /home/model/cosim-model.zip .
+unzip cosim-model.zip
+java -jar /home/cosim/coe-1.0.10-jar-with-dependencies.jar --configuration cosim.json --oneshot --starttime 0.0 --endtime 20.0 1>/home/output/debug.log 2>&1
+cp output.csv /home/output/

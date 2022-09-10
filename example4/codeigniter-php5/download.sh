6d9b1521e79de69084834aa2ebe9930b7a1db85b
prasadtalasila <prasad.talasila@gmail.com>
2022-09-10 18:01:15 +0200

    Adds new example for MiCADO platform
      - Adds new docker files to generate new docker container for running the PHP
        code of GUI container. The generated container downloads required PHP code
        and model files from EGI platform.
      - Adds directory structure to run the DT4Dryer  experiment within MiCADO platform.

---
+++ example4/codeigniter-php5/download.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Download front-end PHP code and model from EGI repository
+code_status=1
+model_status=1
+
+mkdir /home/download
+cd /home/download
+
+while :
+do
+  curl --retry 2 --insecure -H "X-Auth-Token: ${EGI_TOKEN}" "${PHP_CODE_URL}" -o html.zip
+  code_status=$?
+  if [ $code_status -ne 0 ]
+  then 
+    echo "php code is not available yet. Will try again in 60 seconds"
+  fi
+
+  curl --retry 2 --insecure -H "X-Auth-Token: ${EGI_TOKEN}" "${MODEL_URL}" -o cosim-model.zip
+  model_status=$?
+  if [ $model_status -ne 0 ]
+  then 
+    echo "model is not available yet. Will try again in 60 seconds"
+  fi
+
+  if [ $code_status -ne 0 ] || [ $model_status -ne 0 ]
+  then
+    sleep 60
+  else
+    echo -e "\n\nphp code and model are available now. Proceeding with the model evaluation.\n\n"
+    break
+  fi
+done
+
+cp cosim-model.zip /home/model
+cp html.zip /var/www/html
+unzip html.zip -d /var/www/html
